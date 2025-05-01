ee3140e253c2f5fb0fb82d661001ad72f6536acf
Temur662 <temursayfutdinov3@gmail.com>
2025-04-30 20:09:29 -0400

    Set Up Book and appointment button

--- app/page.tsx
+++ app/page.tsx
@@ -324,8 +324,8 @@ export default function Home() {
               <button 
                   className="h-full max-h-[56px] group  px-[32px] py-[16px] rounded-[62px] relative flex items-center  justify-center `md:justify-between bg-[white] text-[#0094E0]  w-full md:w-fit font-[500] text-[14px] "
                   >
-                  Contact Us
-                  <div className='pl-[10px] group-hover:translate-x-3 transition-all duration-300 ease-in-out'>
+                  <p className="group-hover:scale-[1.1] transition-all duration-300 ease-in-out">Contact Us</p>
+                  <div className='pl-[10px] group-hover:translate-x-1 transition-all duration-300 ease-in-out'>
                       <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" viewBox="0 0 18 12" fill="none">
                           <path d="M12.3982 0.768483C12.0402 0.410504 11.4598 0.410506 11.1018 0.768488C10.7438 1.12647 10.7438 1.70687 11.1018 2.06485L14.1203 5.08333H1.66667C1.16041 5.08333 0.75 5.49374 0.75 6C0.75 6.50626 1.16041 6.91667 1.66667 6.91667H14.1203L11.1018 9.93516C10.7439 10.2931 10.7439 10.8735 11.1019 11.2315C11.4598 11.5895 12.0402 11.5895 12.3982 11.2315L16.9766 6.65303C16.9935 6.63637 17.0098 6.61905 17.0254 6.60112C17.0873 6.52997 17.1365 6.45154 17.1728 6.36885C17.2221 6.25677 17.2496 6.13294 17.25 6.00273L17.25 6C17.25 5.99717 17.25 5.99434 17.25 5.99152C17.2489 5.87623 17.2266 5.76602 17.1867 5.66463C17.142 5.55068 17.0736 5.44387 16.9815 5.35178L12.3982 0.768483Z" fill="#0094E0"/>
                       </svg>
