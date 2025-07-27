a9cdc4fd42e2b2a26078547bf7142846a9a4722e
Temur662 <temurbeksayfutdinov@gmail.com>
2025-07-26 21:09:35 -0400

    Fixed Doctors Card

--- components/DoctorCard.tsx
+++ components/DoctorCard.tsx
@@ -28,7 +28,7 @@ export default function DoctorCard({ doctor }: { doctor: DoctorProp }) {
     <Link
       href={`/about/meetourdoctors/${doctor.slug}`}
       aria-label={`View profile for ${doctor.name}`}
-      className='w-full h-full xl:max-h-[630px] hover:shadow-md transition-all duration-300 rounded-[16px] overflow-hidden space-y-[24px] group relative'>
+      className='w-full h-full xl:max-h-[630px] rounded-[16px] overflow-hidden space-y-[24px] group relative'>
       {/* Main clickable card link as an overlay */}
       {/* <Link href={`/about/meetourdoctors/${doctor.slug}`}
         className='absolute inset-0 z-0'
@@ -65,6 +65,20 @@ export default function DoctorCard({ doctor }: { doctor: DoctorProp }) {
             ))
           }
         </div>
+        <button className="bg-white border hover:cursor-pointer border-[#022968] px-[20px] py-[10px] space-x-[10px] flex flex-row items-center justify-center rounded-[62px] w-full mt-auto">
+          <span
+            style={{
+              fontFamily: "var(--font-inter)",
+              fontWeight: 400,
+            }}
+            className="text-[#022968]"
+          >View details</span>
+          <div className=' group-hover:translate-x-2 transition-transform '>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="11" viewBox="0 0 18 11" fill="none">
+              <path d="M12.3982 0.268483C12.0402 -0.0894963 11.4598 -0.089494 11.1018 0.268488C10.7438 0.62647 10.7438 1.20687 11.1018 1.56485L14.1203 4.58333H1.66667C1.16041 4.58333 0.75 4.99374 0.75 5.5C0.75 6.00626 1.16041 6.41667 1.66667 6.41667H14.1203L11.1018 9.43516C10.7439 9.79314 10.7439 10.3735 11.1019 10.7315C11.4598 11.0895 12.0402 11.0895 12.3982 10.7315L16.9766 6.15303C16.9935 6.13637 17.0098 6.11905 17.0254 6.10112C17.0873 6.02997 17.1365 5.95154 17.1728 5.86885C17.2221 5.75677 17.2496 5.63294 17.25 5.50273L17.25 5.5C17.25 5.49717 17.25 5.49434 17.25 5.49152C17.2489 5.37622 17.2266 5.26602 17.1867 5.16463C17.142 5.05068 17.0736 4.94387 16.9815 4.85178L12.3982 0.268483Z" fill="#022968" />
+            </svg>
+          </div>
+        </button>
       </div>
     </Link>
   )
