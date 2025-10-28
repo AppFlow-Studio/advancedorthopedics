89df4f1f87acb9383a375170f49fa671b86be920
Temur662 <temurbeksayfutdinov@gmail.com>
2025-10-27 22:57:05 -0400

    set the correct env key:

--- providers/recaptchaprovider.tsx
+++ providers/recaptchaprovider.tsx
@@ -4,7 +4,7 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3"
 
 export default function RecaptchaProvider({ children }: { children: React.ReactNode }) {
     return (
-        <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY as string} >
+        <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string} >
             {children}
         </GoogleReCaptchaProvider>
     )
